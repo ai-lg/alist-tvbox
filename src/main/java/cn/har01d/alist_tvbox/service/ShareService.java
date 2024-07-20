5e2b636064c0dca49d1e4a7941fce384d59637f8
ai-lg <154646069+ai-lg@users.noreply.github.com>
2024-07-20 08:52:32 +0800

    Update ShareService.java

--- src/main/java/cn/har01d/alist_tvbox/service/ShareService.java
+++ src/main/java/cn/har01d/alist_tvbox/service/ShareService.java
@@ -166,7 +166,7 @@ public class ShareService {
                 cookie = cookie.substring(1, cookie.length() - 1);
                 String purgePan115Temp = lines.get(2).split("=")[1];
                 String dirId = lines.get(3).split("=")[1];
-                // String user_token = Files.readString(Paths.get("/data/mytoken.txt")).trim();
+                String user_token = Files.readString(Paths.get("/data/mytoken.txt")).trim();
         
                 log.info("update storage driver type");
                 Utils.executeUpdate("update x_storages set driver = 'AliyundriveShare2Pan115' where driver = 'AliyundriveShare2Open'");
@@ -174,7 +174,7 @@ public class ShareService {
                 Utils.executeUpdate("update x_storages set addition = json_set(addition, '$.cookie', '" + cookie + "') where driver = 'AliyundriveShare2Pan115'");
                 Utils.executeUpdate("update x_storages set addition = json_set(addition, '$.purge_pan115_temp', '" + purgePan115Temp + "') where driver = 'AliyundriveShare2Pan115'");
                 Utils.executeUpdate("update x_storages set addition = json_set(addition, '$.dir_id', '" + dirId + "') where driver = 'AliyundriveShare2Pan115'");
-                // Utils.executeUpdate("update x_storages set addition = json_set(addition, '$.refresh_token', "\"" + user_token + "\"") where driver = 'AliyundriveShare2Pan115'");
+                Utils.executeUpdate("update x_storages set addition = json_set(addition, '$.refresh_token', '" + user_token + '") where driver = 'AliyundriveShare2Pan115'");
             } catch (Exception e) {
                 throw new BadRequestException(e);
             }
